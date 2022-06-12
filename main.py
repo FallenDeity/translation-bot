12af335907ad3e24795e13a2288c19fa91820700
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2022-06-12 22:03:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -213,10 +213,13 @@ async def translate(ctx, link=None):
     async with aiofiles.open(f'{ctx.author.id}.txt', 'w', encoding='utf-8') as f: await f.write(" ".join(full))
     if os.path.getsize(f"{ctx.author.id}.txt") > 8*10**6:
         c = Client("AXiAEgFvETpKeqBHufPBXz")
-        with zipfile.ZipFile(f'{ctx.author.id}.zip', 'w') as jungle_zip: jungle_zip.write(f'{ctx.author.id}.txt', compress_type=zipfile.ZIP_DEFLATED)
-        filelnk = c.upload(filepath = f"{ctx.author.id}.zip")
+        try:
+            with zipfile.ZipFile(f'{ctx.author.id}.zip', 'w') as jungle_zip: jungle_zip.write(f'{ctx.author.id}.txt', compress_type=zipfile.ZIP_DEFLATED)
+            filelnk = c.upload(filepath = f"{ctx.author.id}.zip")
+            await ctx.reply(f"{name}: here is your novel {filelnk.url}")
+        except:
+            await ctx.reply("sorry your file was too big please split it and try again.")
         os.remove(f"{ctx.author.id}.zip")
-        await ctx.reply(f"{name}: here is your novel {filelnk.url}")
     else:
         file = discord.File(f"{ctx.author.id}.txt", f"{name}.txt")
         await ctx.reply("**🎉Here is your translated novel**", file=file)
