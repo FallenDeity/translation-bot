22fedddb74915471f23834036e5ec879284befda
Asher <61227305+FallenDeity@users.noreply.github.com>
2022-06-28 09:24:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -348,12 +348,14 @@ async def crawl(ctx, link=None):
         return await ctx.reply(f"**We currently crawl only from {', '.join(allowed)}**")
     res = await bot.loop.run_in_executor(None, ask, link)
     novel = {}
-    res = res.text
-    soup = BeautifulSoup(res, 'html.parser')
+    soup = BeautifulSoup(res.text, 'html.parser')
     name = str(link.split('/')[-1].replace('.html', ''))
+    print(name)
     frontend_part = link.replace(f'/{name}', '').split('/')[-1]
     frontend = link.replace(f'/{name}', '').replace(f'/{frontend_part}', '')
+    print(frontend)
     urls = [f'{frontend}{j}' for j in [str(i.get('href')) for i in soup.find_all('a')] if name in j and '.html' in j and 'txt' not in j]
+    print(len(urls))
     maxs = len(urls)
     name = ctx.author.id
     crawler[ctx.author.id] = f'0/{len(urls)}'
