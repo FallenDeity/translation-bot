f0d4968fdc2df4c9d54cccfc44b660c72f1e0035
Asher <61227305+FallenDeity@users.noreply.github.com>
2022-07-04 21:50:32 +0530

    Update translation.py

--- cogs/translation.py
+++ cogs/translation.py
@@ -50,7 +50,7 @@ class Translator(commands.Cog):
         if ctx.message.attachments:
             link = None
         if language not in total and 'http' not in language:
-            return await ctx.reply(f"> **❌We have the following languages in our db.**\n```\n{string}```")
+            return await ctx.reply(f"**❌We have the following languages in our db.**\n```ini\n{string}```")
         if ctx.author.id in self.bot.translator:
             return await ctx.send('> **❌You cannot translate two novels at a time.**')
         if not ctx.message.attachments and not link:
