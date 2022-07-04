78ae85c0ce2a122a2786a65c29e5094dacfc1b48
Smile <ajurahul02@gmail.com>
2022-07-04 20:39:59 +0530

    updated encoding detector

--- cogs/translation.py
+++ cogs/translation.py
@@ -18,7 +18,7 @@ class Translator(commands.Cog):
     def __init__(self, bot: Raizel) -> None:
         self.bot = bot
 
-    def get_encoding_type(self,file):
+
         async with open(file, 'rb') as f:
              rawdata = await f.read()
         return detect(rawdata)['encoding']
@@ -101,7 +101,10 @@ class Translator(commands.Cog):
             async with aiofiles.open(f'{ctx.author.id}.txt', 'r', encoding=j) as f:
                     novel = await f.read()
         except:
-            codec = await self.get_encoding_type(f'{ctx.author.id}.txt')
+            async with open(f'{ctx.author.id}.txt', 'rb') as f:
+                rawdata = await f.read()
+            codec=detect(rawdata)['encoding']
+            # codec = await self.get_encoding_type(f'{ctx.author.id}.txt')
             await ctx.send('Codec found as  '+str(codec))
             try:
                 async with aiofiles.open(f'{ctx.author.id}.txt', 'r', encoding=codec,errors='ignore') as f:
