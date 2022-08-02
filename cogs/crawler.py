41339c0ee6de11cd0f199358c3ca5c7822e57fdb
Smile <ajurahul02@gmail.com>
2022-08-02 23:07:27 +0530

    fixed  error

--- cogs/crawler.py
+++ cogs/crawler.py
@@ -150,7 +150,7 @@ class Crawler(commands.Cog):
             title = f"{ctx.author.id}_crawl"
         # print('titlename'+title_name)
         self.chptitlecss = self.titlecss[1]
-        if title_name == '' or title_name == 'None' or title_name is None or title is None:
+        if title_name == '' or title_name == 'None' or title_name is None :
             title = f"{ctx.author.id}_crawl"
         else:
             title_name = GoogleTranslator(source='auto', target='english').translate(title_name)
