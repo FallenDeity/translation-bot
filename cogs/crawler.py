5bc1a0950f5a3eec2ef16aa32daea0cb8a364d4f
Smile <ajurahul02@gmail.com>
2022-08-04 21:04:07 +0530

    debug fix 69shu

--- cogs/crawler.py
+++ cogs/crawler.py
@@ -184,7 +184,6 @@ class Crawler(commands.Cog):
         print(frontend)
         print([str(i.get('href')) for i in soup.find_all('a')])
         if '69shu' in link:
-            soup = BeautifulSoup(res.content, 'html.parser')
             urls=[f'{j}' for j in [str(i.get('href')) for i in soup.find_all('a')] if
                 name in j and 'http' in j]
         elif 'ptwxz' in link:
