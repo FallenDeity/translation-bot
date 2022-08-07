ed03e5058e5d1d5534fd2633327f6fc1da481a31
Smile <ajurahul02@gmail.com>
2022-08-07 19:39:39 +0530

    tried for biqueabc

--- cogs/crawler.py
+++ cogs/crawler.py
@@ -58,7 +58,7 @@ def findchptitlecss(link):
     if 'uukanshu.cc' in link:
         return ['.booktitle', 'h1 ::text']
     if 'biqugeabc' in link:
-        return [".top>h1", '.reader-main .title ::text']
+        return ['h2', '.reader-main .title ::text']
     if 'uuks' in link:
         return [".jieshao_content>h1", 'h1#timu ::text']
     if 'uukanshu' in link:
