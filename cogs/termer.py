4ceec42e81d6a44280723921419c6c2ff7cb4366
Asher <61227305+FallenDeity@users.noreply.github.com>
2022-08-19 15:27:55 +0530

    Update termer.py

--- cogs/termer.py
+++ cogs/termer.py
@@ -1,4 +1,5 @@
 import os
+import typing
 
 import aiofiles
 import discord
@@ -31,7 +32,7 @@ class Termer(commands.Cog):
         ctx,
         term: str = None,
         link: str = None,
-        file: discord.Attachment = None,
+        file: typing.Optional[discord.Attachment] = None,
         language: str = "english",
     ):
         file = link or file
