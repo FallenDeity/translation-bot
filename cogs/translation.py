87875d461e33572e057336514f1202d538f92c7a
Smile <ajurahul02@gmail.com>
2022-08-20 14:30:02 +0530

    try mega

--- cogs/translation.py
+++ cogs/translation.py
@@ -161,8 +161,8 @@ class Translate(commands.Cog):
     )
     async def mega(self, ctx: commands.Context):
         try:
-            print("userpwd:"+str(os.getenv("USER"))+str(os.getenv('PSW'))+"'")
-            self.bot.mega = Mega().login(email=os.getenv("USER").strip(), password=os.getenv('PSW').strip())
+            print("userpwd:"+str(os.getenv("USER"))+str(os.getenv('PWD'))+"'")
+            self.bot.mega = Mega().login(email=os.getenv("USER").strip(), password=os.getenv('PWD').strip())
             # self.bot.mega = Mega().login(email='9g2sq2es@freeml.net',password='dummy123')
             await ctx.send('Mega login as user was successful')
             user=self.bot.mega.get_user()
