f30e5e7e3ddff2b44683c2c23398497c05c6762f
Smile <ajurahul02@gmail.com>
2022-09-05 18:41:26 +0530

    fixed crawler bug

--- cogs/crawler.py
+++ cogs/crawler.py
@@ -15,7 +15,7 @@ from discord.ext import commands
 from core.bot import Raizel
 from core.views.linkview import LinkView
 from utils.handler import FileHandler
-
+from urllib.parse import urljoin
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
@@ -155,10 +155,10 @@ class Crawler(commands.Cog):
         if link is None:
             return await ctx.reply(f"> **❌Enter a link for crawling.**")
         await ctx.send('Started crawling please wait',delete_after=10)
-        # num = 0
-        # for i in allowed:
-        #     if i not in link:
-        #         num += 1
+        num = 0
+        for i in allowed:
+            if i not in link:
+                num += 1
         # if num == len(allowed):
         #     return await ctx.reply(
         #         f"> **❌We currently crawl only from {', '.join(allowed)}**"
@@ -220,6 +220,7 @@ class Crawler(commands.Cog):
         # print(self.urlcss)
         name = str(link.split("/")[-1].replace(".html", ""))
         # name=name.replace('all','')
+        urls=[]
         frontend_part = link.replace(f"/{name}", "").split("/")[-1]
         frontend = link.replace(f"/{name}", "").replace(f"/{frontend_part}", "")
         if "69shu" in link:
@@ -286,12 +287,22 @@ class Crawler(commands.Cog):
         ):
             urls = urls[::-1]
             # print(urls)
-        if urls==[] or len(urls)<30:
+        # if urls==[] or len(urls)<30:
+        #     urls = [
+        #         f"{frontend}{j}"
+        #         for j in [str(i.get("href")) for i in soup.find_all("a")]
+        #         if name in j and "txt" not in j
+        #     ]
+        if urls == [] or num == len(allowed) or len(urls)<30:
             urls = [
-                f"{frontend}{j}"
+                f"{j}"
                 for j in [str(i.get("href")) for i in soup.find_all("a")]
                 if name in j and "txt" not in j
             ]
+            utemp = []
+            for url in urls:
+                utemp.append(urljoin(link, url))
+            urls = utemp
         if urls == [] or len(urls) < 30:
             link=link+'/'
 
@@ -303,10 +314,15 @@ class Crawler(commands.Cog):
             html = response.text
             sel = parsel.Selector(html)
             urls = [
-                f"{frontend}{j}"
+                f"{j}"
                 for j in sel.css('a ::attr(href)').extract()
                 if name in j and "txt" not in j
             ]
+            utemp=[]
+            for url in urls:
+                utemp.append(urljoin(link, url))
+            urls=utemp
+            # print(urls)
             title_name = sel.css(maintitleCSS + " ::text").extract_first()
             # print(urls)
         if len(urls)<30:
