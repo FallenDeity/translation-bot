a336d208dae580fbd42329d54e2de19c6d79a7d6
Smile <ajurahul02@gmail.com>
2022-09-18 09:06:40 +0530

    added rawname tag

--- utils/handler.py
+++ utils/handler.py
@@ -19,7 +19,6 @@ from languages import languages
 
 
 class FileHandler:
-
     ENCODING: list[str] = ["utf-8", "cp936", "utf-16", "cp949"]
     TOTAL: int = len(ENCODING)
 
@@ -61,7 +60,7 @@ class FileHandler:
         spl = name.split("_")
         dictionary = PyDictionary()
         segment = 0
-        if len(spl)==1:
+        if len(spl) == 1:
             return False
         for t in spl:
             if t is None or t == "":
@@ -69,9 +68,9 @@ class FileHandler:
             if not t[-1].isalpha():
                 t = t[:-1]
             if (
-                t[:-1].isalpha()
-                and len(t) > 3
-                and (bool(dictionary.meaning(str(t), disable_errors=True)) or t.lower() in bot.dictionary)
+                    t[:-1].isalpha()
+                    and len(t) > 3
+                    and (bool(dictionary.meaning(str(t), disable_errors=True)) or t.lower() in bot.dictionary)
             ):
                 if len(t) > 6 or segment == 2:
                     return True
@@ -92,7 +91,7 @@ class FileHandler:
         os.remove(f"{ctx.author.id}.docx")
 
     async def read_file(
-        self, ctx: commands.Context
+            self, ctx: commands.Context
     ) -> typing.Union[str, discord.Message]:
         novel = None
         for i, j in enumerate(self.ENCODING):
@@ -109,10 +108,10 @@ class FileHandler:
                         async with aiofiles.open(f"{ctx.author.id}.txt", "rb") as f:
                             novel = await f.read()
                         async with aiofiles.open(
-                            f"{ctx.author.id}.txt",
-                            "r",
-                            encoding=chardet.detect(novel[:500])["encoding"],
-                            errors="ignore",
+                                f"{ctx.author.id}.txt",
+                                "r",
+                                encoding=chardet.detect(novel[:500])["encoding"],
+                                errors="ignore",
                         ) as f:
                             novel = await f.read()
                     except Exception as e:
@@ -134,10 +133,10 @@ class FileHandler:
         return string
 
     async def distribute(
-        self, bot: Raizel, ctx: commands.Context, name: str, language: str, original_language: str
+            self, bot: Raizel, ctx: commands.Context, name: str, language: str, original_language: str, raw_name: str
     ) -> None:
         download_url = None
-        if (size := os.path.getsize(f"{ctx.author.id}.txt")) > 8 * 10**6:
+        if (size := os.path.getsize(f"{ctx.author.id}.txt")) > 8 * 10 ** 6:
             try:
                 await ctx.send(
                     "Translation Completed... Your novel is too big.We are uploading to Mega.. Please wait",
@@ -154,7 +153,7 @@ class FileHandler:
                 channel = bot.get_channel(1005668482475643050)
                 user = str(ctx.author)
                 await channel.send(
-                    f"> {name.replace('_',' ')} \nuploaded by {user} Translated from: {original_language} to: {language}",
+                    f"> {name.replace('_', ' ')} \nuploaded by {user} Translated from: {original_language} to: {language}",
                     view=view,
                 )
                 download_url = filelnk
@@ -167,11 +166,11 @@ class FileHandler:
         else:
             file = discord.File(f"{ctx.author.id}.txt", f"{name}.txt")
             await ctx.reply("**🎉Here is your translated novel**", file=file)
-            guild=bot.get_guild(940866934214373376)
+            guild = bot.get_guild(940866934214373376)
             channel = guild.get_channel(1005668482475643050)
             user = str(ctx.author)
             msg = await channel.send(
-                f'> {name.replace("_"," ")} \nUploaded by {user} Translated from: {original_language} to: {language}',
+                f'> {name.replace("_", " ")} \nUploaded by {user} Translated from: {original_language} to: {language}',
                 file=discord.File(f"{ctx.author.id}.txt", f"{name}.txt"),
             )
             os.remove(f"{ctx.author.id}.txt")
@@ -180,7 +179,9 @@ class FileHandler:
             except:
                 pass
             download_url = msg.attachments[0].url
-        if download_url and size> 0.3 * 10**6:
+        if raw_name is not None:
+            name = name + "__" + raw_name
+        if download_url and size > 0.3 * 10 ** 6:
             novel_data = [
                 await bot.mongo.library.next_number,
                 name,
@@ -197,13 +198,12 @@ class FileHandler:
             data = Novel(*novel_data)
             await bot.mongo.library.add_novel(data)
 
-
     async def crawlnsend(
-        self, ctx: commands.Context, bot: Raizel, title: str, title_name: str, originallanguage: str
+            self, ctx: commands.Context, bot: Raizel, title: str, title_name: str, originallanguage: str
     ) -> None:
         download_url = None
-        if (size := os.path.getsize(f"{title}.txt")) > 8 * 10**6:
-            if size > 25 * 10**6:
+        if (size := os.path.getsize(f"{title}.txt")) > 8 * 10 ** 6:
+            if size > 25 * 10 ** 6:
                 os.remove(f"{title}.txt")
                 await ctx.send('Crawled file is too big. there is some problem in crawler')
             try:
@@ -232,7 +232,7 @@ class FileHandler:
                 file.close()
             except:
                 pass
-        if download_url and size> 0.3 * 10**6:
+        if download_url and size > 0.3 * 10 ** 6:
             novel_data = [
                 await bot.mongo.library.next_number,
                 title_name,
@@ -248,4 +248,3 @@ class FileHandler:
             ]
             data = Novel(*novel_data)
             await bot.mongo.library.add_novel(data)
-
