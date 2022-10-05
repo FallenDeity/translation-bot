a32be3b0d457ebeede513ac738fb8ce9adb7a869
Smile <ajurahul02@gmail.com>
2022-10-05 16:42:32 +0530

    moved the selectors to selector.py

--- cogs/crawler.py
+++ cogs/crawler.py
@@ -23,96 +23,11 @@ from cogs.library import Library
 from cogs.translation import Translate
 from core.bot import Raizel
 from utils.handler import FileHandler
+from utils.selector import CssSelector
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
 }
-
-
-def findURLCSS(link):
-    if "bixiange" in link:
-        return "p ::text"
-    elif "sj.uukanshu" in link or "t.uukanshu" in link:
-        return "#read-page p ::text"
-    elif "uukanshu.cc" in link:
-        return ".bbb.font-normal.readcotent ::text"
-    elif "uukanshu" in link:
-        return ".contentbox ::text"
-    elif (
-            "trxs.me" in link
-            or "trxs.cc" in link
-            or "qbtr" in link
-            or "tongrenquan" in link
-    ):
-        return ".read_chapterDetail p ::text"
-    elif "biqugeabc" in link:
-        return ".text_row_txt >p ::text"
-    elif "uuks" in link:
-        return "div#contentbox > p ::text"
-    elif "jpxs" in link:
-        return ".read_chapterDetail p ::text"
-    elif "powanjuan" in link or "ffxs" in link or "sjks" in link:
-        return ".content p::text"
-    elif "txt520" in link:
-        return "div.content > p ::text"
-    elif "69shu" in link:
-        return ".txtnav ::text"
-    elif "ptwxz" in link:
-        return "* ::text"
-    elif "shu05" in link:
-        return "#htmlContent ::text"
-    elif "readwn" in link:
-        return ".chapter-content ::text"
-    elif "novelsemperor" in link:
-        return "div.epcontent.entry-content > p ::text"
-    elif "www.vbiquge.co" in link:
-        return "#rtext ::text"
-    elif "2015txt.com" in link:
-        return "#cont-body ::text"
-    elif "4ksw.com" in link:
-        return "div.panel-body.content-body.content-ext ::text"
-    else:
-        return "* ::text"
-
-
-def findchptitlecss(link):
-    if (
-            "trxs.me" in link
-            or "trxs.cc" in link
-            or "tongrenquan" in link
-            or "qbtr" in link
-            or "jpxs" in link
-    ):
-        return [".infos>h1:first-child", ""]
-    if "bixiange" in link:
-        return [".desc>h1", ""]
-    if "powanjuan" in link or "ffxs" in link:
-        return ["title", ""]
-    if "sjks" in link:
-        return [".box-artic>h1", ""]
-    if "sj.uukanshu" in link or "t.uukanshu" in link:
-        return [".bookname", "#divContent >h3 ::text"]
-    if "uukanshu.cc" in link:
-        return [".booktitle", "h1 ::text"]
-    if "biqugeabc" in link:
-        return ["title", "title ::text"]
-    if "uuks" in link:
-        return [".jieshao_content>h1", "h1#timu ::text"]
-    if "uukanshu" in link:
-        return ["title", "h1#timu ::text"]
-    if "69shu" in link:
-        return [".bread>a:nth-of-type(3)", "title ::text"]
-    if "ptwxz" in link:
-        return [".title", "title ::text"]
-    else:
-        return ["title", "title ::text"]
-
-
-def find_next_selector(link):
-    if "readwn" in link or "wuxiax.co" in link or "novelmt.com" in link:
-        return "#chapter-article > header > div > aside > nav > div.action-select > a.chnav.next"
-    else:
-        return None
 
 
 class Crawler(commands.Cog):
@@ -315,7 +230,7 @@ class Crawler(commands.Cog):
             data = await res.read()
             soup1 = BeautifulSoup(data, "lxml", from_encoding=res.get_encoding())
 
-        self.titlecss = findchptitlecss(link)
+        self.titlecss = CssSelector.findchptitlecss(link)
         maintitleCSS = self.titlecss[0]
         try:
             title_name = str(soup1.select(maintitleCSS)[0].text)
@@ -344,7 +259,7 @@ class Crawler(commands.Cog):
         self.chptitlecss = self.titlecss[1]
 
         if selector is None:
-            self.urlcss = findURLCSS(link)
+            self.urlcss = CssSelector.findURLCSS(link)
         else:
             if not '::text' in selector:
                 selector = selector + " ::text"
@@ -619,7 +534,7 @@ class Crawler(commands.Cog):
                 "> **❌You cannot crawl two novels at the same time.**"
             )
         if nextselector is None:
-            nextselector = find_next_selector(firstchplink)
+            nextselector = CssSelector.find_next_selector(firstchplink)
             if nextselector is not None:
                 secondchplink = None
                 cloudscrape = True
@@ -631,7 +546,7 @@ class Crawler(commands.Cog):
             if '::text' not in css:
                 css += ' ::text'
         else:
-            css = '* ::text'
+            css = CssSelector.findURLCSS(firstchplink)
         if noofchapters is None:
             noofchapters = 2000
         try:
