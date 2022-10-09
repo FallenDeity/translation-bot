de0a60cfd226d199d9fa3e3c7c35edf212591b56
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2022-10-09 06:36:26 +0530

    Fixed error

--- cogs/translation.py
+++ cogs/translation.py
@@ -66,7 +66,7 @@ class Translate(commands.Cog):
         file_type = None
         name = None
         rep_msg = await ctx.reply("Please wait.. Translation will began soon")
-        if "discord.com/channels" in link:
+        if link is not None and "discord.com/channels" in link:
             messageid = link
             link = None
         if ctx.message.attachments:
