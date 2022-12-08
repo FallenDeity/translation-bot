5df49c33621d9ee060dd44dd8a668c56c94fc35e
Smile <ajurahul02@gmail.com>
2022-12-08 20:51:25 +0530

    changed to 1 hr

--- core/bot.py
+++ core/bot.py
@@ -126,7 +126,7 @@ class Raizel(commands.Bot):
         langs = list(self.languages.keys()) + list(self.languages.values())
         return langs
 
-    @tasks.loop(hours=4)
+    @tasks.loop(hours=1)
     async def auto_restart(self):
         i = 0
         if self.auto_restart.current_loop != 0:
