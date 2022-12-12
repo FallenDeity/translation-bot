035f9913d21e435d52afb14d97b80f6178802d62
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2022-12-12 06:41:37 +0530

    Update termer.py
    Fixed a error in termer

--- cogs/termer.py
+++ cogs/termer.py
@@ -221,7 +221,7 @@ class Termer(commands.Cog):
             name_lib_check = False
             for n in novel_data:
                 ids.append(n._id)
-                if "english" == n.language.lower():
+                if "english" == n.language:
                     eng_check = True
                 if language == n.language:
                     lang_check = True
