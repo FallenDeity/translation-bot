e732f3a1d7fe73295a03d2256b6aff1d58eac26b
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2023-03-18 18:52:40 +0530

    added add_role command

--- cogs/general.py
+++ cogs/general.py
@@ -53,14 +53,15 @@ class General(commands.Cog):
         guild = self.bot.get_guild(940866934214373376)
         role = guild.get_role(1076124121592770590)
         await ctx.send("started adding roles")
-        top = await self.bot.mongo.library.get_user_novel_count(_top_150=True)
+        top = await self.bot.mongo.library.get_user_novel_count(_top_200=True)
         top_200 = [(user_id, count) for user_id, count in top.items()]
         chunks = [top_200[i: i + 10] for i in range(0, len(top_200), 10)]
         user_ids = []
         no = 0
         for chunk in chunks:
             for user_id, count in chunk:
-                user_ids.append(user_id)
+                if count >= 10:
+                    user_ids.append(user_id)
         members = guild.members
         for member in members:
             if member.id in user_ids:
