873ed6454591da92f5ae9bd508826d7fef1a3243
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2023-06-27 20:39:34 +0530

    added wuxiap

--- cogs/crawler.py
+++ cogs/crawler.py
@@ -1018,7 +1018,7 @@ class Crawler(commands.Cog):
             sel = parsel.Selector(response.text)
             soup = BeautifulSoup(response.content, 'html5lib', from_encoding=response.encoding)
             secondchplink = await FileHandler.find_next_chps(soup, firstchplink)
-        if "readwn" in firstchplink or "wuxiax.co" in firstchplink or "novelmt.com" in firstchplink or "fannovels.com" in firstchplink or "novelmtl.com" in firstchplink or "booktoki216.com" in firstchplink or "69shu" in firstchplink:
+        if "readwn" in firstchplink or "wuxiax.co" in firstchplink or "novelmt.com" in firstchplink or "fannovels.com" in firstchplink or "novelmtl.com" in firstchplink or "booktoki216.com" in firstchplink or "69shu" in firstchplink or "wuxiap.com" in firstchplink:
             waittime = 1.0
         if "69shu" in firstchplink:
             waittime = 1.65
