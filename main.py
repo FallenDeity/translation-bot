9ea803757b6f924f13d5ee96706f0ffc2b4a9aa2
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2023-07-02 17:00:21 +0530

    changed census method

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 
 import discord
 from discord.ext import tasks
+from utils.handler import FileHandler as handler
 
 from core.bot import Raizel
 
@@ -10,13 +11,7 @@ bot = Raizel()
 
 @tasks.loop(seconds=120)
 async def census():
-    await bot.change_presence(
-        activity=discord.Activity(
-            type=discord.ActivityType.listening,
-            name=f"{len(bot.users)} novel enthusiasts. Prefix: .t",
-        ),
-        status=discord.Status.idle,
-    )
+    await handler.update_status(bot)
 
 
 @census.before_loop
