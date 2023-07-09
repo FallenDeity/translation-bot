886431479afc4372f7ddfa64a2263562ac19d903
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2023-07-09 13:39:02 +0530

    chan scr

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -1,4 +1,4 @@
 cd /home/ubuntu/
 rm -rf translation-bot
-git clone  git@github.com:Ajurahul/translation-bot.git >>/home/ubuntu/logs
+git clone  git@github.com:Ajurahul/translation-bot.git
 echo "$USER : Updated git. bot will restart after this">>/home/ubuntu/logs
