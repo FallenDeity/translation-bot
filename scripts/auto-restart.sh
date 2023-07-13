4119389a02283a9a29ad82c665e5e5f28244838b
aju.k <ajurahul02@gmail.com>
2023-07-13 16:11:00 +0530

    changed script added sleep

--- scripts/auto-restart.sh
+++ scripts/auto-restart.sh
@@ -2,13 +2,13 @@ PID=`ps -ef | grep -v grep | grep "python3 main.py"| awk '{print $2}'`
 
 if ! ps -p $PID > /dev/null
 then
-  nowtime=$(date)
-
   pkill -f tmux
   killall python3
-  pgrep procname && killall python3
+  sleep 1
+  pgrep python3 && killall python3
   tmux new-session -d -s ENTER
   tmux detach -s ENTER
+  nowtime=$(date)
   tmux send-keys -t 0 "cd /home/ubuntu/translation-bot;python3 main.py" ENTER
   echo "$USER : started bot at $nowtime">>/home/ubuntu/logs
 #else
--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -1,4 +1,5 @@
 cd /home/ubuntu/
 rm -rf translation-bot
+sleep 1
 git clone  git@github.com:Ajurahul/translation-bot.git
 echo "$USER : Updated git. bot will restart after this">>/home/ubuntu/logs
