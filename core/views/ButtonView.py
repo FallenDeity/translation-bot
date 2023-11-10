2296911ce8c94e264d5bbdfcf50f6b420bbfd494
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2023-11-10 21:04:41 +0530

    fix bug

--- core/views/ButtonView.py
+++ core/views/ButtonView.py
@@ -11,21 +11,25 @@ class ButtonsV(discord.ui.View):
         self.ctx = ctx
         self.task = task
 
+    async def check_user_task(self, interaction):
+        # Check if the user is the one who initiated the task
+        if self.ctx.author.id != interaction.user.id:
+            await interaction.response.send_message("Different user.")
+            return False
+        return True
+
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.success, emoji='❌')
     async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.ctx.author.id != interaction.user.id:
-            await interaction.response.send_message("different user")
-        elif self.task == "crawlnext" and self.ctx.author.id in self.bot.crawler_next:
+        if not await self.check_user_task(interaction):
+            return
+        if self.task == "crawlnext" and self.ctx.author.id in self.bot.crawler_next:
             self.bot.crawler_next[self.ctx.author.id] = "break"
+            await interaction.response.send_message("Stopping the crawl next task.")
         elif self.task == "crawl" and self.ctx.author.id in self.bot.crawler:
             self.bot.crawler[self.ctx.author.id] = "break"
+            await interaction.response.send_message("Stopping the crawl task.")
         elif self.task == "translate" and self.ctx.author.id in self.bot.translator:
             self.bot.translator[self.ctx.author.id] = "break"
-        if self.ctx.author.id == interaction.user.id:
-            return await interaction.response.send_message("Stopping the task")
-
-
-
-    # @discord.ui.button(label="No", style=discord.ButtonStyle.danger, emoji='👎')
-    # async def downvote_button(self, button : discord.ui.Button, interaction : discord.Interaction):
-    #     pass
+            await interaction.response.send_message("Stopping the translation task.")
+        else:
+            await interaction.response.send_message("No active task to cancel.")
