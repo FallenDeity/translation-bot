1176dd8563edac8f06a3188858a56492288ecf11
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2023-12-02 13:37:34 +0530

    added add novel

--- cogs/general.py
+++ cogs/general.py
@@ -1,10 +1,14 @@
 import datetime
+import typing
 
 import discord
 from discord.ext import commands
 
 from core.bot import Raizel
 from core.views.linkview import LinkView
+from databases.data import Novel
+from utils.category import Categories
+from utils.handler import FileHandler
 
 
 class General(commands.Cog):
@@ -53,6 +57,43 @@ class General(commands.Cog):
         await ctx.send("started adding roles")
         n = await self.bot.add_roles()
         return await ctx.send(f"added storage-access role to {n} users")
+
+    @commands.hybrid_command(help="add novel to library")
+    async def addnovel(self, ctx: commands.Context, file: typing.Optional[discord.Attachment] = None, link: str = None, name: str = ""):
+        if file is None and link is None:
+            await ctx.reply("add a file or link")
+
+        if file:
+            link = file.url
+            if name.strip() == "":
+                name = file.filename.replace(".txt", "").replace(".docx", "").replace(".epub", "").replace(".pdf", "")
+        category = Categories.from_string(name)
+        no = await self.bot.mongo.library.next_number
+        novel_data = [
+            no,
+            name,
+            "",
+            0,
+            "english",
+            await FileHandler.get_tags(name),
+            link,
+            999999,
+            ctx.author.id,
+            datetime.datetime.now(datetime.timezone.utc).timestamp(),
+            # datetime.datetime.utcnow().timestamp(),
+            Categories.thumbnail_from_category(category),
+            "english",
+            category,
+            link
+        ]
+        data = Novel(*novel_data)
+        try:
+            await self.bot.mongo.library.add_novel(data)
+            await ctx.send(f"> added to bot with library id {no}")
+        except:
+            await ctx.send("couldn't add to library try again")
+
+
 
 
 async def setup(bot: Raizel) -> None:
