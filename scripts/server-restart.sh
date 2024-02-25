d1a84efc82bae6c3c592d43f4520ab1b914d3e2e
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2024-02-25 08:37:22 +0530

    added server restart

--- scripts/server-restart.sh
+++ scripts/server-restart.sh
@@ -1,4 +1,4 @@
 nowtime=$(date)
 echo "$USER : Restarting server at $nowtime">>/home/ubuntu/logs
-aws ec2 reboot-instances --instance-ids i-025c10c1ed58f97b3 --profile admin
+aws ec2 reboot-instances --instance-ids i-025c10c1ed58f97b3 --profile admin >>/home/ubuntu/logs
 sudo systemctl reboot
